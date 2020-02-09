3809686e5d29246c6612d6bf319a7a29878964a6
Micheal Kayode <kayodemicheal159@gmail.com>
2020-02-09 15:21:06 +0100

    initial commit from CRA

---
+++ src/containers/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './header/Header';
+import Nav from './nav/Nav';
+import Main from './main/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <section className="aside-main">
+        <Nav />
+
+        <Main />
+      </section>
+    </div>
+  );
+};
+
+export default App;
