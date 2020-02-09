c1122dfef96c54d6b93f9b9bfde5960a47d36deb
Micheal Kayode <kayodemicheal159@gmail.com>
2020-02-09 16:01:45 +0100

    implement the nav container

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './header/Header';
+import Nav from './nav/Nav';
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <section className="aside-main">
+        <Nav />
+      </section>
     </div>
   );
 };
---
+++ src/containers/nav/Nav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Nav.css';
+import analytics from '../../assets/imgs/analytics.svg';
+import happy from '../../assets/imgs/happy.svg';
+import schedule from '../../assets/imgs/schedule.svg';
+import comment from '../../assets/imgs/comment.svg';
+
+const Nav = () => (
+  <aside className="menu">
+    <a href="#analytics" className="menu__item">
+      <img src={analytics} alt="analytics menu item"/>
+    </a>
+
+    <a href="#happy" className="menu__item">
+      <img src={happy} alt="happy menu item"/>
+    </a>
+
+    <a href="#schedule" className="menu__item">
+      <img src={schedule} alt="schedule menu item"/>
+    </a>
+
+    <a href="#comment" className="menu__item">
+      <img src={comment} alt="comment menu item"/>
+    </a>
+  </aside>
+);
+
+export default Nav;
